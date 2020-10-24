f727fa3082a11ff8725abbb6aebe85b37544fff9
cousinskeeta <jacob.tadesse@gmail.com>
2020-10-23 23:57:43 -0400

    updated

--- app.py
+++ app.py
@@ -2,21 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite://///Users/jacobtadesse/Documents/practice/databases/issues.db'
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:////Users/jacobtadesse/Documents/practice/databases/issues.db'
 db = SQLAlchemy(app)
 
 class issues(db.Model):
-	__tablename__ = 'issues'
-	id = db.Column('id', db.Integer, primary_key=True)
-	zip = db.Column('zip', db.String)
-	issue1 = db.Column('issue1', db.String)
-	issue2 = db.Column('issue2', db.String)
-	issue3 = db.Column('issue3', db.String)
-	date = db.Column('date', db.String)
+	id = db.Column(db.Integer, primary_key=True)
+	zipcode = db.Column(db.String(7))
+	issue1 = db.Column(db.String(240))
+	issue2 = db.Column(db.String(240))
+	issue3 = db.Column(db.String(240))
+	date = db.Column(db.String(10))
 
-	def __init__(self, id, zip, issue1, issue2, issue3, date):
-		self.id = id
-		self.zip = zip
+	def __init__(self, zipcode, issue1, issue2, issue3, date):
+		# self.id = id
+		self.zipcode = zipcode
 		self.issue1 = issue1
 		self.issue2 = issue2
 		self.issue3 = issue3
