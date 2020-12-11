56ef86f535906aa49781577425ae9abb0d79e2b8
cousinskeeta <jacob.tadesse@gmail.com>
2020-12-10 20:11:04 -0500

    auth

--- IssuesAndSolutions/project/auth.py
+++ IssuesAndSolutions/project/auth.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, logout_user, login_required
-from .models import User
+from flask import render_template, redirect, url_for, request, flash # Blueprint
+# from werkzeug.security import generate_password_hash, check_password_hash
+# from flask_login import login_user, logout_user, login_required
+from flask_security import, Blueprint, hash_password, login_required, login_user, logout_user, SQLAlchemyUserDatastore, ch
+from flask_security.utils import verify_password
+from .models import User, Role
 from . import db
 
 auth = Blueprint('auth', __name__)
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 
 @auth.route('/login')
 def login():
@@ -16,7 +19,8 @@ def login_post():
 	password = request.form['password']
 	remember = True if request.form.get('remember') else False
 	user = User.query.filter_by(email=email).first()
-	if not user or not check_password_hash(user.password, password):
+	if not user or not verify_password(user.password)
+	if not user or not check_password_hash(password, user.password):
 		flash('Please check your login details and try again.')
 		return redirect(url_for('auth.login'))
 
@@ -31,7 +35,7 @@ def signup():
 def signup_post():
 	email = request.form.get('email')
 	name = request.form.get('name')
-	password = request.form.get('password')
+	password = hash_password(request.form.get('password'))
 
 	user = User.query.filter_by(email=email).first()
 
@@ -39,9 +43,13 @@ def signup_post():
 		flash('Email address already exists')
 		return redirect(url_for('auth.signup'))
 
-	new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
-	
-	db.session.add(new_user)
+	# new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+	# db.session.add(new_user)
+
+	user_datastore.create_user(
+		email = email,
+		password = password
+	)
 	db.session.commit()
 
 	return redirect(url_for('auth.login'))
@@ -51,3 +59,4 @@ def signup_post():
 def logout():
 	logout_user()
 	return redirect(url_for('main.index'))
+
