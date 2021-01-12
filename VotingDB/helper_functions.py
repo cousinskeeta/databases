bbf7267638f64762dcada53992213fc6b9f58962
cousinskeeta <jacob.tadesse@gmail.com>
2021-01-12 18:25:27 -0500

    voting db
    ;
    ;

---
+++ VotingDB/helper_functions.py
@@ -0,0 +1,11 @@
+def salt_password(password):
+    iterations = 0
+    password_array = []
+    for char in password:
+        iterations += 1
+        password_array.append(char)
+        if iterations % 2 == 0:
+            password_array.append("$*")
+    return "".join(password_array)
+
+
